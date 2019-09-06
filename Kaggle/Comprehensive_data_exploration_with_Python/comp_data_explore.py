4a77567ade77418383d0b9bd59a5df430a578cc1
Manish-Lalwani <54809501+Manish-Lalwani@users.noreply.github.com>
2019-09-06 23:17:12 +0530

    Update comp_data_explore.py

--- Kaggle/Comprehensive_data_exploration_with_Python/comp_data_explore.py
+++ Kaggle/Comprehensive_data_exploration_with_Python/comp_data_explore.py
@@ -27,14 +27,14 @@ def import_split_dataset_func():
 
 
 #choosing features
-
-
 #splitting numerical and categorical data
 
 #call  set_categorical_cols = split_num_categorical_cols_func(x_train)
 def split_num_categorical_cols_func(x_train):   
-    all_cols = x_train.columns # getting all column names
-    num_cols = x_train._get_numeric_data().columns #getting column nmes of numeric column
+    #all_cols = x_train.columns # getting all column names
+    #num_cols = x_train._get_numeric_data().columns #getting column nmes of numeric column
+    all_cols = ds_train.columns # getting all column names
+    num_cols = ds_train._get_numeric_data().columns #getting column nmes of numeric column
 
     ###using set as with list was getting error as we cannot subtract 2 list bt we can subtract two set###
     set_all_cols = set(all_cols)
@@ -54,9 +54,8 @@ def split_num_categorical_cols_func(x_train):
 	for x in set_num_cols: 
     	print(x)
     	temp_index = ds_train.columns.get_loc(x)
-    
     	if temp_index != 80 : # have put this condition because as the y i.e sales price is also numerical and we were getting that columns indexx also but when imputer ws performed we were getting out of bound issue
-        	numeric_cols_index.append(temp_index)#appending to list
+            numeric_cols_index.append(temp_index)#appending to list
 
     return categorical_cols_index,numeric_cols_index
 
@@ -78,9 +77,33 @@ for x in numeric_cols_index:
 
 
 #fixing categorical data
+ds_train[list(set_categorical_cols)] = ds_train[list(set_categorical_cols)].fillna(ds_train.mode().iloc[0])
 
+
+#fixing space in column 3rd column MsZoning
+###temp_store = x_train[:,2]
+###x_train[:,2] = np.char.strip(x_train[:,2]," ")
+###np.core.defchararray.strip(x_train[:,2], chars=None)
+###np.char.replace(x_train[:,2]," ","")
+ds_train.iloc[:,2] = ds_train.iloc[:,2].str.replace(" ","") #worked
+
+
+#label encoder
+#converting categorical to numeric
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_obj_x = LabelEncoder()
+
+#labelencoder doesnot takes 2d array it only takes single column at at ime so
+for x in categorical_cols_index:
+    print("column is ",x)
+    x_train[:,x] = labelencoder_obj_x.fit_transform(x_train[:,x]) 
+
+#onehotencoder
+    onehotencoder_x = OneHotEncoder(categorical_features=categorical_cols_index)
+    x_train = onehotencoder_x.fit_transform(x_train).toarray()
 #trainig model and predicting
 def multi_lin_reg_train_func(x_train,y_train):
+    from sklearn.linear_model import LinearRegression
     multi_lin_reg = LinearRegression()
     multi_lin_reg.fit(x_train,y_train)
     y_pred = multi_lin_reg.predict(x_train)
